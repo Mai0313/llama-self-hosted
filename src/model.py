5f1742dc31b32dce6e0b2818d0ae22fbe5a9e34c
mtk30765 <wei.chenglee@mediatek.com>
2023-10-26 18:16:51 +0800

    feat: update model selection and top_k values
    - Change the value of `model_selection` from `"meta-llama/Llama-2-7b-chat-hf"` to `"meta-llama/Llama-2-13b-chat-hf"`
    - Change the value of `top_k` from `10` to `1`
    
    Signed-off-by: mtk30765 <wei.chenglee@mediatek.com>

--- src/model.py
+++ src/model.py
@@ -7,8 +7,8 @@ from omegaconf import OmegaConf
 rootutils.setup_root(__file__, indicator=".project-root", pythonpath=True)
 
 
-model_selection = "meta-llama/Llama-2-7b-chat-hf"
-# model_selection = "meta-llama/Llama-2-13b-chat-hf"
+# model_selection = "meta-llama/Llama-2-7b-chat-hf"
+model_selection = "meta-llama/Llama-2-13b-chat-hf"
 tokenizer = AutoTokenizer.from_pretrained(model_selection, cache_dir = "./models")
 pipeline = transformers.pipeline(
     "text-generation",
@@ -21,7 +21,7 @@ def generate_response(input_text):
     sequences = pipeline(
         input_text,
         do_sample=True,
-        top_k=10,
+        top_k=1,
         num_return_sequences=1,
         eos_token_id=tokenizer.eos_token_id,
         max_length=200,
